5fc3249b27bb40794d67cf91ec8d27835ec31dfa
Matthew Ho <matthew.ho@okta.com>
2021-07-27 17:36:58 -0700

    System dialog on success

--- clean_build.sh
+++ clean_build.sh
@@ -21,3 +21,4 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 printf "\tBuild completed successfully!!! \n" >> cron.log
 printf "=================================================================================\n" >> cron.log
+osascript -e 'tell app "System Events" to display dialog "Build process completed with exit code 0"'
